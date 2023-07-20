d505ee0167c6436baf15ded694297f891f745c95
Sarah-Fisher <sarah.fisher@mygeorgian.ca>
2023-07-20 17:35:28 -0400

    commit queries

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Assignment 2 | COMP 1073 Client-Side Javascript
+
+/* Variables
+-------------------------------------------- */
+
+const size = document.querySelector("#size");
+const crust = document.querySelector("#crust");
+const sauce = document.querySelector("#sauce");
+const sauceAmount = document.querySelector("#sauce_amount");
+const cheese = document.querySelector("#cheese");
+const toppings = document.querySelector("#toppings[]");
+
+const orderDesc = document.querySelector("#orderDesc");
+const studentId = document.querySelector("#studentId");
+
+const order = document.querySelector("order");
+
+
+
+
+
+
